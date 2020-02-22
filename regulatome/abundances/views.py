a46cbe73e579735e3676c604a5015eca21c18257
kemaeleon <kemaeleon@gmail.com>
2020-02-22 09:36:05 +0000

    another working reduced version

--- regulatome/abundances/views.py
+++ regulatome/abundances/views.py
@@ -24,7 +24,7 @@ from django_tables2 import MultiTableMixin, RequestConfig, SingleTableMixin, Sin
 from django_tables2.export.views import ExportMixin
 from django_tables2.paginators import LazyPaginator
 from .tables import PeakTable, IndexTable, SingleTimePointTable, GeneTable, MultiTimeTable, SingleTimeTable
-from .filter import TimeCourseFilter, SingleTimePointFilter, IndexFilter, GeneFilter,Gene, MultiTimeFilter
+from .filter import GeneFilter,Gene
 from django.http import JsonResponse
 from django.shortcuts import redirect
 
@@ -44,47 +44,6 @@ class GeneSearch(ExportMixin, SingleTableMixin, FilterView):
         return {"template_name": "django_tables2/bootstrap.html"}
 
 
-
-class FilterListView(ExportMixin, SingleTableMixin, FilterView):
-    model = TimeCourse
-    table_class = PeakTable
-    filterset_class = TimeCourseFilter
-    template_name = "bootstrap_template.html"
-
-    export_formats = ("csv", "xls")
-
-    def get_queryset(self):
-        return super().get_queryset()
-
-    def get_table_kwargs(self):
-        return {"template_name": "django_tables2/bootstrap.html"}
-
-class SingleTimePointListView(ExportMixin, SingleTableMixin, FilterView):
-    model = SingleTimePoint
-    table_class = SingleTimePointTable 
-    filterset_class = SingleTimePointFilter
-    template_name = "bootstrap_template_sp.html"
-
-    export_formats = ("csv", "xls")
-
-    def get_queryset(self):
-        return super().get_queryset()
-
-    def get_table_kwargs(self):
-        return {"template_name": "django_tables2/bootstrap.html"}
-
-
-
-class OverView(ExportMixin, SingleTableMixin,FilterView):
-    model = IndexAbundance
-    table_class = IndexTable
-    filterset_class = IndexFilter
-    template_name = "bootstrap_template2.html"
-
-    def get_queryset(self):
-        return super().get_queryset()
-    def get_table_kwargs(self):
-        return {"template_name": "django_tables2/bootstrap.html"}
 
 
 class DV(APIView):
@@ -106,21 +65,6 @@ def random_color():
     lst = [rd.choice(string.hexdigits) for n in range(6)]
     hex_number = "#" +  "".join(lst)
     return hex_number
-
-
-
-class TableResultView(ExportMixin, SingleTableMixin, FilterView):
-        table_class = MultiTimeTable
-        filterset_class = MultiTimeFilter
-        template_name = "bootstrap_template.html"
-
-        export_formats = ("csv", "xls")
-
-        def get_queryset(self):
-            return super().get_queryset()
-
-        def get_table_kwargs(self):
-            return {"template_name": "django_tables2/bootstrap.html"}
 
 
 
@@ -169,92 +113,14 @@ def  multitimeview(request):
     return render(request, 'sp_tc.html', context)
 
 
-def singletimeview(request):
-    if request.method == "POST":
-        pks = request.POST.getlist("selected")
-        selected_objects = MultiTime.objects.filter(pk__in=pks)
-        for i in selected_objects:
-            setattr(i, 'uniq_gene_id', i.gene_id.gene_id + "_" +  i.gene_id.accession)
-            i.save()
-
-        selected_objects = SingleTimePoint.objects.filter(gene_id__in=pks).values()
-        selected_geneids = SingleTimePoint.objects.filter(gene_id__in=pks).values_list('gene_id',flat=True)
-        context = {}
-        context['uniq_gene_id'] = list(selected_geneids)
-        context['data_stp'] = json.dumps(list(selected_objects))
-    return render(request, 'singletimepoint.html', context)
-
-
-
-
-
-def timecourseview(request):
-    if request.method == "POST":
-        pks = request.POST.getlist("selected")
-        selected_objects = TimeCourse.objects.filter(pk__in=pks).values()
-        selected_geneids = TimeCourse.objects.filter(pk__in=pks).values_list('uniq_gene_id',flat=True)
-        context = {}
-        context['uniq_gene_id'] = list(selected_geneids)
-        print(context['uniq_gene_id'])
-        context['data_tc'] = json.dumps(list(selected_objects))
-        print("HOHO", context['data_tc'])
-        context['data_stp'] = None
-    return render(request, 'sp_tc.html', context)
-
-
-def singletimepointview(request):
-    if request.method == "POST":
-        pks = request.POST.getlist("selected")
-        selected_objects = SingleTimePoint.objects.filter(pk__in=pks).values()
-        selected_geneids = SingleTimePoint.objects.filter(pk__in=pks).values_list('uniq_gene_id',flat=True)
-        context = {}
-        context['uniq_gene_id'] = list(selected_geneids)
-        context['data_stp'] = json.dumps(list(selected_objects))
-    return render(request, 'singletimepoint.html', context)
 
 def announce(request):
     response = "<html><body><h1>Regulatome@Matheson Lab. coming soon.</h1></body></html>"
     return HttpResponse(response)
 
 
-def redirectindex(request):
-    if request.method == "POST":
-        pks=request.POST.getlist("selected")
-        label = ['resultindex']
-        newurl = '/' + '_'.join(label+pks) + '/'
-        response = redirect(newurl)
-        return response
-
 def flatten(listoflists):
     return [item for liste in listoflists for item in liste]
-
-def indexview(request):
-    if request != "":
-        # obtain IndexAbundance primary keys from URL
-        pks = [int(i) for i in re.split('/|_', str(request.get_full_path())) if i.isdigit() ]
-        # obtain pks of TimeCourse and SingleTimePoint objects 
-        
-        # get time course data
-        tc = [i['time_course'] for i in list(IndexAbundance.objects.filter(pk__in=pks).values('time_course'))]
-        l2 = flatten(tc)
-        sel_timecourse = TimeCourse.objects.filter(pk__in=l2).values() 
-        selected_gene_tc = TimeCourse.objects.filter(pk__in=l2).values_list('uniq_gene_id', flat=True)
-
-        # get single point data
-        selected_sp =[i['single_time_point'] for i in list(IndexAbundance.objects.filter(pk__in=pks).values('single_time_point'))]
-        l2 = flatten(selected_sp)
-        sel_stp = SingleTimePoint.objects.filter(pk__in=l2).values() 
-        selected_gene_spt = SingleTimePoint.objects.filter(pk__in=l2).values_list('uniq_gene_id', flat=True)
-
-        selected_geneids = list(selected_gene_tc) + list(selected_gene_spt)
-        selected_geneids.sort()
-       
-        context = {}
-        context['uniq_gene_id'] = selected_geneids
-        context['data_tc'] = json.dumps(list(sel_timecourse))
-        context['data_stp'] = json.dumps(list(sel_stp))
-    return render(request, 'sp_tc.html', context)
-
 
 
 def dicegame(request):
